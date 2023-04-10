ab796d6dda4052cd85f8dc0ba02c2cc65038d9d1
呂 博多 <blyu@ryonoMacBook-Air.local>
2023-04-10 17:15:30 +0900

    enemy

--- srcs/enemy/move_enemy.c
+++ srcs/enemy/move_enemy.c
@@ -2,18 +2,21 @@
 #include "define.h"
 #include "utils.h"
 
+static int a_enemy_move(t_info *i, t_character *enemy);
+
 int	enemy_move(t_info *i)
 {
 	size_t  l;
 	size_t	r;
 
 	l = 0;
-	r = i->enermy_num;
-	while (l < i->enermy_num)
+	r = i->enemy_num;
+	while (l < i->enemy_num)
 	{
-		if (i->enermy[l].is_alive)
+		if (i->enemy[l].is_alive)
 		{
-			a_enemy_move(i, i->enermy + l);
+			if (a_enemy_move(i, i->enemy + l))
+				i->player.is_alive = false;
 		}
 		else
 			r--;
@@ -22,7 +25,7 @@ int	enemy_move(t_info *i)
 	return (!r);
 }
 
-int a_enemy_move(t_info *i, t_character *enemy)
+static int a_enemy_move(t_info *i, t_character *enemy)
 {
 	double len;
 	t_pos v;
@@ -30,29 +33,15 @@ int a_enemy_move(t_info *i, t_character *enemy)
 	v.x = i->player.pos.x - enemy->pos.x;
 	v.y = i->player.pos.y - enemy->pos.y;
 	len = (v.x * v.x) + (v.y * v.y);
-	if (len < ENEMY_FINND_LEN * ENEMY_FINND_LEN && enemy_sight(i))
+	if (len < ENEMY_FIND_LEN * ENEMY_FIND_LEN && enemy_sight(i, enemy))
 	{
-		enemy->time--;
-		if (enemy->time == 0)
+		if (enemy->time >= ENEMY_FIND_TIME)
 			return (1);
-		else if (enemy->time < ENEMY_FIRE_TIME)
+		else if (enemy->time >= ENEMY_FIRE_TIME)
 			enemy->yaw = vector_to_8direction(v);
+		enemy->time++;
 	}
 	else
-		enemy->time = ENEMY_FINND_TIME;
+		enemy->time = 0;
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
